6ea232aadfea1a413d2e97d40964069a8c730159
Suj018300 <sujalbendre2526@gmail.com>
2026-01-04 14:53:27 +0530

    SingleThreading

---
+++ SingleThreading/Client.java
@@ -0,0 +1,35 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+
+    public void run() throws IOException, UnknownHostException {
+        int port = 8080;
+        InetAddress address = InetAddress.getByName("localhost");
+        Socket socket = new Socket(address, port);
+        PrintWriter toSocket = new PrintWriter(socket.getOutputStream());
+        BufferedReader fromSocket = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        toSocket.println("Hello from the Client");
+        String line = fromSocket.readLine();
+
+        System.out.println("Response from the socket :" + line);
+        toSocket.close();
+        fromSocket.close();
+        socket.close();
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        try {
+            client.run();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
